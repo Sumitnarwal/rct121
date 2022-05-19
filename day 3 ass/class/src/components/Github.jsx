d8013de0fe69a36c8374d50897bb2b8c18cfc7a5
Sumitnarwal <sumitnarwal39@gmail.com>
2022-05-19 12:42:15 +0530

    update

--- day 3 ass/class/src/components/Github.jsx
+++ day 3 ass/class/src/components/Github.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react"
 import { SerachBox } from "./Searchbox"
 import { ShowData } from "./show.data"
 
-const getData = (q = "react", page = 1) => {
-    return axios("https://api.github.com/search/repositories", {
+const getData = (q = "react", page = 1, sort = "asc") => {
+    return axios(`https://api.github.com/search/repositories?_sort=${sort}`, {
         method: "GET",
         params: {
             q,
             per_page: 6,
-            page
+            page,
+            sort
+
         }
     })
 }
@@ -17,30 +19,48 @@ export const Github = () => {
     const [repo, setRep] = useState([])
     const [page, setPage] = useState(1);
     const [query, setQuery] = useState("React")
+    const [sort, setSort] = useState("desc")
     //  console.log(repo)
 
     useEffect(() => {
-        getData(query, page).then((res) => {
+        getData(query, page, sort).then((res) => {
             setRep(res.data);
             console.log("repo", res.data)
         }).catch((err) => {
             console.log(err)
         })
-    }, [query])
-    const handleChange=(query)=>{
+    }, [query, page, sort])
+
+    const handleChange = (query) => {
         setQuery(query)
         console.log(query)
     }
+
+    const handleSet = () => {
+        setSort("asc")
+    }
+
+    const handleSet1 = () => {
+        setSort("desc")
+    }
+    
     return (
         <div>
             <h2>Github Users</h2>
-            <SerachBox handleChange={handleChange}/>
+            <SerachBox handleChange={handleChange} />
             <div id="repo">
                 {repo?.items?.map((item) => (
                     <ShowData key={item.id} {...item} />
                 ))}
+                <div >
+                    <button disabled={page === 1} onClick={() => setPage(page - 1)}>Prev</button>
+                    <button onClick={() => setPage(page + 1)}>Next</button>
+                    <button onClick={handleSet}>sort</button>
+                    <button onClick={handleSet1}>sort low to high</button>
+                    <div id="pageno" >{page}{" "}page out of{" "}{(repo.total_count / repo.items).toFixed(0)}</div>
+                </div>
             </div>
-           
+
         </div>
     )
 }
